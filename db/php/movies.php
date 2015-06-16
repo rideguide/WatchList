fa35c85d6c623078c8b7d530ef69dbb7f99a1cbc
rideguide <maximdurig@maxims-air.tbz.local>
2015-06-16 15:43:43 +0200

    Reorganisation Commit

---
+++ db/php/movies.php
@@ -0,0 +1,19 @@
+<?php
+	$host = "localhost";
+	$un = "root";
+	$pw = "";
+	$dbname = "movieDb";
+	
+	$db = new mysqli($host, $un, $pw, $dbname);
+
+
+	$query = "select * from movies";
+	$res = $db->query($query);
+	
+	$a = array();
+	while ($row = $res->fetch_assoc()) 
+	{	
+		$a[] = $row;
+	}
+	echo json_encode($a);
+?>
