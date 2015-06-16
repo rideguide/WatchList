878f15367caa9caff17c3f49ba8d357c50d84009
rideguide <maximdurig@maxims-air.tbz.local>
2015-06-16 10:25:09 +0200

    Initial Commit

---
+++ db/dump.sql
@@ -0,0 +1,49 @@
+drop database movieDb;
+create database movieDb;
+
+use movieDb;
+
+create table movies 
+(
+	id_pk int NOT NULL AUTO_INCREMENT,
+ 	name varchar(801),
+ 	directorId_fk int,
+ 	description text,
+ 	genere_fk int,
+ 	primary key (id_pk)
+ );
+
+create table episodes 
+(
+	id_pk int NOT NULL AUTO_INCREMENT,
+ 	serieId_fk int,
+ 	serieNr int,
+ 	episodeNr int,
+ 	name varchar(801),
+ 	primary key (id_pk)
+ );
+
+create table user 
+(
+	id_pk int NOT NULL AUTO_INCREMENT,
+ 	username varchar(801),
+ 	password varchar(64),
+ 	status boolean,
+ 	primary key (id_pk)
+ );
+
+create table genere 
+(
+	id_pk int NOT NULL AUTO_INCREMENT,
+ 	name varchar(801),
+ 	primary key (id_pk)
+ );
+
+create table watchlist 
+(
+	id_pk int NOT NULL AUTO_INCREMENT,
+ 	user_fk int,
+ 	film_fk int,
+ 	rating int,
+ 	primary key (id_pk)
+ );
