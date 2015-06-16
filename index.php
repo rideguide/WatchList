1f3f4835d6cf3884ea5be987abdaad050f1df095
Simon Mena <simon_mena@bluewin.ch>
2015-06-16 13:53:12 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html>
+    
+    <head>
+        
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+
+        
+    </head>
+    <body>
+        
+        
+        <div data-role="page" data-theme="a">  <!-- page -->
+            
+            <div data-role="header"data-theme="b">    <!-- header -->
+                <h1>WatchList</h1>
+            </div>  <!-- header -->
+            
+            <div data-role="content">   <!-- content -->
+                <p><h4>E-Mail</h4>
+				<input type="email" name="email">
+				<h4>Passwort</h4>
+				<input type="password" name="passwort"></p>
+				<input value="Anmelden" type="submit" name="anmelden" />
+            </div>  <!-- content -->
+            
+            <div data-role="footer">    <!-- footer -->
+                
+            </div>  <!-- footer -->
+            
+        </div>  <!-- page -->
+        
+        
+        
+    </body>
+    
+</html>
