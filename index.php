37498d5fb321b66d80a1a3e9c5d5e8978396037d
Simon Mena <simon_mena@bluewin.ch>
2015-06-17 16:36:31 +0200

    update

--- index.php
+++ index.php
@@ -12,7 +12,12 @@
         <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
         <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
         <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+		<script src="js/star-rating/jquery.form.js"></script>
+		<script src="js/star-rating/jquery.MetaData.js"></script>
+		<script src="js/star-rating/jquery.rating.pack.js"></script>
+		<script src="js/star-rating/jquery.rating.js"></script>
 		
+		<link rel="stylesheet" href="js/star-rating/jquery.rating.css" />
 		<link rel="stylesheet" href="css/main.css" />
         <link rel="stylesheet" href="css/menu.css" />
 
