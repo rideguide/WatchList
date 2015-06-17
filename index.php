89883c4dc224581afcb898c4f19c5e7312c3ad94
Simon Mena <simon_mena@bluewin.ch>
2015-06-17 15:29:50 +0200

    update commit

--- index.php
+++ index.php
@@ -3,15 +3,18 @@
 <html>
     
     <head>
-        
-
-		<meta content="width=device-width, initial-scale=1" name="viewport">
+	    <meta http-equiv="cleartype" content="on">
+		<meta name="MobileOptimized" content="320">
+		<meta name="HandheldFriendly" content="True">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+		
         <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
         <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
         <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-		<script src="js/transition.js"></script>
-        <link rel="stylesheet" href="css/menu.css" />
+		
 		<link rel="stylesheet" href="css/main.css" />
+        <link rel="stylesheet" href="css/menu.css" />
 
 		
     </head>
@@ -20,7 +23,7 @@
     <div data-role="page" id="login" data-url="login" data-theme="a">  <!-- page login -->
             
             <div data-role="header"data-theme="b">    <!-- header -->
-                <h1>WatchList</h1>
+                <h1>WatchList</h1>				
             </div>  <!-- header -->
             
             <div data-role="content">   <!-- content -->
