a40b0b2c1e83ecc89e05ff02b4c6e199b5c60466
Simon Mena <simon_mena@bluewin.ch>
2015-06-17 13:02:25 +0200

    update commit

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
     
     <head>
         
+
+		<meta content="width=device-width, initial-scale=1" name="viewport">
         <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
         <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
         <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 		<script src="js/transition.js"></script>
         <link rel="stylesheet" href="css/menu.css" />
 		<link rel="stylesheet" href="css/main.css" />
-
 
 		
     </head>
